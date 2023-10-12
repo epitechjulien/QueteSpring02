3203bef6549993c65d1a0f35c12a612a5d6c2988
Julien Moukmir <mouk20@hotmail.fr>
2023-10-12 19:13:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QueteSpring02
---
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -0,0 +1,29 @@
+package com.wildcodeschool.doctor.controller;
+
+import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+
+@Controller
+public class DoctorController {
+
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor doctor(@PathVariable int number) {
+        Doctor doctor;
+        if (number == 13) {
+            doctor = new Doctor(number, "Jodie Whittaker");
+        }   else if (number >= 1 && number <= 12) {
+            // doctor = new Doctor(number, "ENTRE 1 ET 12");
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Erreur 303");
+        }   else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+        }
+        return doctor;
+    }
+}
